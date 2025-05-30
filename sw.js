38e0f3120c8832ce5a73e526ed49a0c055a4aac1
ashishchahat <princeboofer786@gmail.com>
2025-05-31 01:05:51 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
 
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        './',
+        './index.html',
+        './manifest.json',
+        './ghost.png',
+        './ghost_1.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
