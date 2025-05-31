b35c65b122cb6bfe55e9fd226333f5ce9b5e83c3
ashishchahat <princeboofer786@gmail.com>
2025-05-31 15:24:26 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
 
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+});
