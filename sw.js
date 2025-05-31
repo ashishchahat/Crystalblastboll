900077d9c853c38a3d74fe2dd68f93ab7e1afd39
ashishchahat <princeboofer786@gmail.com>
2025-05-31 16:00:16 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,42 @@
 
+const CACHE_NAME = 'crystal-blast-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/sw.js',
+  '/icon.png',
+  '/splash-screen.png',
+  '/ghost.png',
+  '/ghost_1.png',
+  '/break_boll.mp3',
+  '/blast_1.mp3',
+  '/freez.mp3',
+  '/powerup.mp3',
+  '/ding.mp3',
+  '/game_over.mp3',
+  '/cashregister.mp3',
+  '/aww.mp3',
+  '/animewow.mp3',
+  '/bonncrack.mp3',
+  '/kidsaye.mp3',
+  '/gear.mp3'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
